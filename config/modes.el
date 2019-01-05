1355af9921e9455f25b81131b1774e3f068a8c6a
Ole Strøm <olestr@pm.me>
2019-01-05 01:02:14 +0100

    improve after-load hooks names

--- config/modes.el
+++ config/modes.el
@@ -5,14 +5,14 @@
 
 
 (let ((directory (file-name-directory load-file-name)))
-  (with-eval-after-load 'electric
+  (with-eval-after-load "elec-pair"
     (absolute-load (concat directory "electric-pair.el")))
 
-  (with-eval-after-load 'term
+  (with-eval-after-load "term"
     (absolute-load (concat directory "term-mode/init.el")))
 
-  (with-eval-after-load 'info
+  (with-eval-after-load "info"
     (absolute-load (concat directory "info-mode.el")))
 
-  (with-eval-after-load 'custom
+  (with-eval-after-load "cus-edit"
     (absolute-load (concat directory "custom-mode.el"))))
