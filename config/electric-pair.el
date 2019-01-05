1796b9d67363a873f4f952545130ad2da7968775
Ole Strøm <olestr@pm.me>
2019-01-05 18:55:45 +0100

    fix electric-pair hook for elisp-mode

--- config/electric-pair.el
+++ config/electric-pair.el
@@ -1,12 +1,14 @@
 (defun create-pair(string)
   (cons (string-to-char string) (string-to-char string)))
 
-(setq single-quote (create-pair "'"))
+(defvar single-quote (create-pair "'"))
 
 
 (add-to-list 'electric-pair-pairs single-quote)
 (setq electric-pair-delete-adjacent-pairs nil)
 
 
-(with-eval-after-load 'elisp-mode
-  (setq-local electric-pair-pairs (remove single-quote electric-pair-pairs)))
+(add-hook
+ 'emacs-lisp-mode-hook
+ (lambda()
+   (setq-local electric-pair-pairs (remove single-quote electric-pair-pairs))))
