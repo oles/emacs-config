886aceb98fe5600eac4dfd226dee08784a5c7525
Ole Strøm <olestr@pm.me>
2019-08-16 14:12:52 +0200

    feat: prevent keybindings for tabbar-mode when in tabbar-local-mode

--- config/modes/tabbar-mode/key-bindings.el
+++ config/modes/tabbar-mode/key-bindings.el
@@ -1,20 +1,50 @@
+;; -*- lexical-binding: t -*-
+
+
 (dolist (keymap '(tabbar-mode tabbar-mwheel-mode))
   (setq minor-mode-map-alist
         (assq-delete-all keymap minor-mode-map-alist)))
 
 
-(global-set-key [C-tab] 'tabbar-forward-tab)
-(global-set-key [C-S-iso-lefttab] 'tabbar-backward-tab)
+(global-set-key
+ [C-tab]
+ (lambda()
+   (interactive)
+   (unless tabbar-local-mode (tabbar-forward-tab))))
 
-(global-set-key (kbd "M-<left>") (lambda() (interactive) (tabbar-move-tab)))
-(global-set-key (kbd "M-<right>") (lambda() (interactive) (tabbar-move-tab t)))
+(global-set-key
+ [C-S-iso-lefttab]
+ (lambda()
+   (interactive)
+   (unless tabbar-local-mode (tabbar-backward-tab))))
 
-(global-set-key (kbd "M-1") (lambda() (interactive) (tabbar-select 0)))
-(global-set-key (kbd "M-2") (lambda() (interactive) (tabbar-select 1)))
-(global-set-key (kbd "M-3") (lambda() (interactive) (tabbar-select 2)))
-(global-set-key (kbd "M-4") (lambda() (interactive) (tabbar-select 3)))
-(global-set-key (kbd "M-5") (lambda() (interactive) (tabbar-select 4)))
-(global-set-key (kbd "M-6") (lambda() (interactive) (tabbar-select 5)))
-(global-set-key (kbd "M-7") (lambda() (interactive) (tabbar-select 6)))
-(global-set-key (kbd "M-8") (lambda() (interactive) (tabbar-select 7)))
-(global-set-key (kbd "M-9") (lambda() (interactive) (tabbar-select 8)))
+
+
+(global-set-key
+ (kbd "M-<left>")
+ (lambda()
+   (interactive)
+   (unless tabbar-local-mode (tabbar-move-tab))))
+
+(global-set-key
+ (kbd "M-<right>")
+ (lambda()
+   (interactive)
+   (unless tabbar-local-mode (tabbar-move-tab t))))
+
+
+(let ((create-pair-from (number-sequence 1 9)))
+
+  (let ((pairs (mapcar (lambda(number)
+                         (cons
+                          (concat "M-" (number-to-string number))
+                          (1- number)))
+                       create-pair-from)))
+    (dolist (pair pairs)
+
+      (global-set-key
+       (kbd (car pair))
+       (lambda()
+         (interactive)
+         (unless tabbar-local-mode
+           (tabbar-select (cdr pair))))))))
