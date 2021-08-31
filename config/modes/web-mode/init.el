0644efd5ba184c77a07bdac806b7722ce9abde84
Ole Strøm <olestr@pm.me>
2021-08-31 13:53:39 +0200

    feat: use web-mode for php files

--- config/modes/web-mode/init.el
+++ config/modes/web-mode/init.el
@@ -1,8 +1,9 @@
 ;; -*- lexical-binding: t -*-
 
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.twig\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.twig\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
 
 (let ((directory (file-name-directory load-file-name)))
   (with-eval-after-load 'web-mode
