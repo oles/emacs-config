e736ef8a4dd7bd62d9c12fb1579718397d68db6a
Ole Strøm <olestr@pm.me>
2021-10-06 17:12:37 +0200

    feat: add process check for active processess in term-mode

--- config/modes/term-mode/behaviour.el
+++ config/modes/term-mode/behaviour.el
@@ -1,38 +1,9 @@
-;; TODO: add hook to kill processes without child processes on kill-emacs-query-functions
-
-
-;; (setq kill-buffer-query-functions
-;;       (delq 'process-kill-buffer-query-function kill-buffer-query-functions))
-
-
-
-;; (add-hook
-;;  'kill-buffer-query-functions
-;;  (lambda ()
-;;    (if (y-or-n-p "test query!")
-;;        (progn
-;;          (message "test")
-;;          nil)
-;;      t)))
-
-;; (add-hook 'kill-buffer-query-functions process-kill-buffer-query-function)
-
-
-;; (add-hook
-;;  'kill-emacs-query-functions
-;;  (lambda ()
-;;    (if (y-or-n-p "test query!")
-;;        (progn
-;;          (message "test")
-;;          nil)
-;;      t)))
-
-
-;; (defun my-tracing-function (proc string)
-;;   (message "Proc %S received %S" proc string))
-
-;; (add-function :before (process-filter proc) #'my-tracing-function)
-
-
-;; (defadvice process-kill-buffer-query-function (before check-terminal-processes activate)
-;;   (message "test 123"))
+(advice-add
+ 'save-buffers-kill-emacs
+ :before
+ (lambda()
+   (dolist (buffer (buffer-list))
+     (with-current-buffer buffer
+       (when (and (string-equal major-mode "term-mode")
+                  (= 1 (length (get-terminal-processes))))
+         (set-process-query-on-exit-flag (get-buffer-process buffer) nil))))))
